0df5418a8cf3c30e883f8dfa54c128a100886c3b
Daniel Valenzuela <danielamigos@gmail.com>
2015-08-27 07:54:45 -0500

    final changes i hope

--- wp-content/themes/SaintsAndSinnersHalf/functions.php
+++ wp-content/themes/SaintsAndSinnersHalf/functions.php
@@ -562,6 +562,46 @@ function saintsandsinners_add_editor_styles() {
 }
 add_action( 'init', 'saintsandsinners_add_editor_styles' );
 
+
+// 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
+ 
+function my_acf_settings_path( $path ) {
+ 
+    // update path
+    $path = get_stylesheet_directory() . '/acf/';
+    
+    // return
+    return $path;
+    
+}
+ 
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+ 
+function my_acf_settings_dir( $dir ) {
+ 
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/acf/';
+    
+    // return
+    return $dir;
+    
+}
+ 
+
+// 3. Hide ACF field group menu item
+//add_filter('acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/acf/acf.php' );
+
+
+
+
+
 if( function_exists('acf_add_options_page') ) {
 	
 	acf_add_options_page(array(
@@ -580,7 +620,6 @@ if( function_exists('acf_add_options_page') ) {
 }
 
 require_once('wp_bootstrap_navwalker.php');
-
 
 
 ?>
